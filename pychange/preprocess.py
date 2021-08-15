3a17835f3479f5ca51572c37a5c91c2d566fb0f2
Jacob Gursky <gurskyjacob@gmail.com>
2021-08-15 11:33:09 -0500

    Worked on performance

--- pychange/preprocess.py
+++ pychange/preprocess.py
@@ -1,19 +1,24 @@
 # Importing packages
 import numpy as np
+from numba import njit
 
+@njit(['f8[:, ::1](f8[::1])', 'f4[:, ::1](f4[::1])'], fastmath=True)
 def create_summary_stats(x):
     n = x.shape[0]
-    sum_stats = np.stack((np.append(0, x.cumsum()),
-                          np.append(0, (x ** 2).cumsum()),
-                          np.append(0, ((x - x.mean()) ** 2).cumsum()),
-                          np.arange(0, n + 1)),
+    start_val = x.dtype.type(0)
+    sum_stats = np.stack((np.append(start_val, x.cumsum()),
+                          np.append(start_val, (x ** 2).cumsum()),
+                          np.append(start_val, ((x - x.mean()) ** 2).cumsum()),
+                          np.arange(0, n + 1, dtype=x.dtype)
+                         ),
                          axis=-1)
     return sum_stats
 
+@njit(['f8[:, ::1](f8[::1], i4)', 'f4[:, ::1](f4[::1], i4)'], fastmath=True)
 def create_partial_sums(x, k):
 
     n = x.shape[0]
-    partial_sums = np.zeros(shape=(n + 1, k), dtype=np.float64)
+    partial_sums = np.zeros(shape=(n + 1, k), dtype=x.dtype)
     sorted_data = np.sort(x)
 
     for i in np.arange(k):
--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@ import numpy
 from setuptools import setup
 #from setuptools.extension import Extension
 #from Cython.Build import cythonize
-from pychange.costs import cc
 
 if __name__ == '__main__':
-
-    # Compiling numba extensions first
-    cc.compile()
     
     # Running setup
     setup(
         name='pychange',
         version='0.0.1',
         packages=['pychange'],
-        ext_modules= [cc.distutils_extension()],# + cythonize(Extension('cython_costs', ['pychange/cython_costs.pyx'])),
+        #ext_modules= [cc.distutils_extension()],# + cythonize(Extension('cython_costs', ['pychange/cython_costs.pyx'])),
         install_requires=['numpy', 'numba', 'ruptures'],
         #include_dirs=[numpy.get_include()]
     )
