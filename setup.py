5e7bc5aa78b81055de1978ff1dee6f2acfa4bc3e
Jacob Gursky <gurskyjacob@gmail.com>
2022-05-03 20:22:35 -0500

    Updated build scripts

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 if __name__ == '__main__':
     
     # Running setup
     setup(
         name='fastchange',
-        version='0.0.1a',
+        version='1.0.1',
+        author="Jacob Gursky",
+        author_email='gurskyjacob@gmail.com',
+        description="Fast change point detection in Python",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         packages=find_packages(),
         install_requires=['numpy', 'numba'],
         extras_require={
